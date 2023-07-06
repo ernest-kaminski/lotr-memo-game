7a1e3930d34607ad7158ba0317717d9f962d4686
ernestrator <ernestrator@int.local>
2023-07-06 14:58:13 +0200

    06-07-2023 A

--- components/card.js
+++ components/card.js
@@ -8,60 +8,95 @@ import Data from './cardsData'
 
 function Card(){
     const [numberOfFlippedCards, setNumberOfFlippedCards] = useState(0);
+    const [firstFlippedCardId, setFirstFlippedCardId] = useState(0);
+    const [secondFlippedCardId, setSecondFlippedCardId] = useState(0);
+    const [cardsData, setCardsData] = useState(Data)
+
+    function incrementNumberOfFlippedCards(){
+        setNumberOfFlippedCards((prevValue) => prevValue + 1)   
+    }
+
     useEffect(() => {
-        console.log("number of flipped cards: " + numberOfFlippedCards)
-    }, [numberOfFlippedCards])
+        setCardsData(Data)
+    }, [])
+
+    useEffect(() => {
+        if(numberOfFlippedCards == 2){    
+            setTimeout(() => {
+                compareCards()
+            }, 2000)       
+            setNumberOfFlippedCards(0);
+        }
+    }, [cardsData])
+
+    const compareCards = () => {
+        if(firstFlippedCardId !== secondFlippedCardId){
+            const newState = cardsData.map(card => {
+                if((card.cardId == firstFlippedCardId && card.isFlipped) || (card.cardId == secondFlippedCardId && card.isFlipped) ){
+                        return {...card, isFlipped: false}
+                }
+                return card
+            })
+            setCardsData(newState)
+        }else{
+            const newState = cardsData.filter(card => card.cardId != firstFlippedCardId)
+            setCardsData(newState)
+        }
+
+        setFirstFlippedCardId(0)
+        setSecondFlippedCardId(0)
+    }
+
+    const onClicked = (cardId, id) => {
+        const newState = cardsData.map(card => {
+            if(card.cardId == cardId && card.id == id && !card.isFlipped){
+                    return {...card, isFlipped: true}
+            }
+            return card
+        })
+        setCardsData(newState)
+
+        if(numberOfFlippedCards <= 1 ){
+
+            if(numberOfFlippedCards == 0){
+                setFirstFlippedCardId(cardId)
+            }else if(numberOfFlippedCards == 1){
+                setSecondFlippedCardId(cardId)
+            }
+            incrementNumberOfFlippedCards()
+        }
+    }
+
+    const cards = cardsData.map((card, index) => {
+        return(
+        <div className={styles.card} key={card.id}>
+            <div className={ card.isFlipped ? `${styles.cardinner}  ${styles.isflipped}` : styles.cardinner } onClick={() => onClicked(card.cardId, card.id)} id={card.id}>
+                <div className={`${styles.cardface} ${styles.cardfacefront}`}>
+                <Image src={Ring} alt="" className={styles.ring}/>
+                <div className={styles.cardfacetext}>
+                    <h2>Check Your Card</h2>
+                </div>
+                </div>
+                <div className={`${styles.cardface} ${styles.cardfaceback}`}>
+                <div className={styles.cardcontent}>
+                    <div className={styles.cardheader}>
+                    <Image src={card.image} alt="" className={styles.gandalf} />
+                    <h2>{card.name}</h2>
+                    </div>
+                    <div className={styles.cardbody}>
+                    <h3>{card.title}</h3>
+                    <p>{card.description}</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>)
+    })
 
     return(
         <body>
-            {Data.map((card) => {
-
-
-                const [isClicked, setClicked] = useState(false);
-                function incrementNumberOfFlippedCards(){
-                    setNumberOfFlippedCards((prevValue) => prevValue + 1)   
-                }
-                function decrementNumberOfFlippedCards(){
-                    setNumberOfFlippedCards((prevValue) => prevValue - 1)   
-                }
-                const onClicked = () => {
-                    if(numberOfFlippedCards <= 1 ){
-                        isClicked ? decrementNumberOfFlippedCards() : incrementNumberOfFlippedCards()                              
-                        setClicked((prevValue) => !prevValue)           
-                    }else{
-                        if(isClicked){
-                            decrementNumberOfFlippedCards()
-                            setClicked((prevValue) => !prevValue)   
-                        } 
-                    }
-                }
-
-
-                return(
-                <div className={styles.card}>
-                    <div className={ isClicked ? `${styles.cardinner}  ${styles.isflipped}` : styles.cardinner } onClick={onClicked}>
-                        <div className={`${styles.cardface} ${styles.cardfacefront}`}>
-                        <Image src={Ring} alt="" className={styles.ring}/>
-                        <div className={styles.cardfacetext}>
-                            <h2>Check Your Card</h2>
-                        </div>
-                        </div>
-                        <div className={`${styles.cardface} ${styles.cardfaceback}`}>
-                        <div className={styles.cardcontent}>
-                            <div className={styles.cardheader}>
-                            <Image src={card.image} alt="" className={styles.gandalf} />
-                            <h2>{card.name}</h2>
-                            </div>
-                            <div className={styles.cardbody}>
-                            <h3>{card.title}</h3>
-                            <p>{card.description}</p>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                </div>)
-            })}
-    </body>
+            {cards}
+        </body>
     )
 }
 
--- components/cardsData.js
+++ components/cardsData.js
@@ -6,25 +6,58 @@ import Legolas from '../images/legolas.jpg'
  const data = [
      {
         id: 1,
+        cardId: 1,
         name: "Gandalf The Grey",
         title: "Powerfull Wizard",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id erat a magna lobortis dictum. Nunc est arcu, lacinia quis sapien placerat, laoreet tincidunt nulla.",
-        image: Gandalf
+        image: Gandalf,
+        isFlipped: false
     },
     {
         id: 2,
+        cardId: 2,
         name: "Aragorn son of Arathorn",
         title: "King of Gondor",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id erat a magna lobortis dictum. Nunc est arcu, lacinia quis sapien placerat, laoreet tincidunt nulla.",
-        image: Aragorn
+        image: Aragorn,
+        isFlipped: false
     },
     {
         id: 3,
+        cardId: 3,
         name: "Legolas from Mirkwood",
         title: "Prince of Wooden Elves",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id erat a magna lobortis dictum. Nunc est arcu, lacinia quis sapien placerat, laoreet tincidunt nulla.",
-        image: Legolas
-    }
+        image: Legolas,
+        isFlipped: false
+    },
+    {
+        id: 4,
+        cardId: 2,
+        name: "Aragorn son of Arathorn",
+        title: "King of Gondor",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id erat a magna lobortis dictum. Nunc est arcu, lacinia quis sapien placerat, laoreet tincidunt nulla.",
+        image: Aragorn,
+        isFlipped: false
+    },
+    {
+        id: 5,
+        cardId: 3,
+        name: "Legolas from Mirkwood",
+        title: "Prince of Wooden Elves",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id erat a magna lobortis dictum. Nunc est arcu, lacinia quis sapien placerat, laoreet tincidunt nulla.",
+        image: Legolas,
+        isFlipped: false
+    },
+    {
+        id: 6,
+        cardId: 1,
+        name: "Gandalf The Grey",
+        title: "Powerfull Wizard",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id erat a magna lobortis dictum. Nunc est arcu, lacinia quis sapien placerat, laoreet tincidunt nulla.",
+        image: Gandalf,
+        isFlipped: false
+    },
 
  ]
 
